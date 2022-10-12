84bed9e70d66566a16e7ffc977ee13e5c2063ad8
NidhiKanwar <nidhikanwar45@gmail.com>
2022-10-12 15:08:37 +0530

    crud todo_list added

--- src/components/AddTodo/AddTodo.js
+++ src/components/AddTodo/AddTodo.js
@@ -1,32 +1,41 @@
 import {useState} from "react";
 import {useDispatch} from "react-redux";
-import cuid from "cuid";
 import {addTodo} from "../../Action/todoAction";
+import cuid from "cuid";
 
-
-const AddTodo = ()=>{
-    const [tasks , setTasks] = useState(" ");
-    const dispatch = useDispatch()
-    const handleChange = (e)=>{
-        setTasks(e.target.value);
-        console.log(tasks);
+const AddTodo = () =>{
+    const [tasks , setTasks] = useState("");
+    const dispatch = useDispatch();
+    function handleChange(e) {
+        setTasks(e.target.value)
     }
 
-    const handleSubmit = (e) =>{
+    function handleSubmit(e) {
         e.preventDefault();
-       dispatch(addTodo({task: tasks, id : cuid()}))
+        // eslint-disable-next-line no-undef
+        if (!tasks.length || /^(\w+\s)*\s*$/i.test(tasks) ) {
+            return;
+        }
+        setTasks(dispatch(addTodo({task : tasks , id : cuid()})));
         e.target.userInput.value = "";
+        // console.log(tasks)
+
     }
+
     return(
+        <>
         <form onSubmit={handleSubmit}>
             <input
-                type = 'text'
-                name='userInput'
-                placeholder="Enter your task..."
-                onChange={(e) =>handleChange(e)}/>
-            <button type='submit'>ADD</button>
-        </form>
-    )
-}
+            type='text'
+            name = 'userInput'
+            placeholder="Enter your task.."
+            onChange={(e) => handleChange(e)}
+            />
 
-export default AddTodo;
+            <button type = 'submit'>Add</button>
+        </form>
+        </>
+    )
+  }
+
+  export default AddTodo;
--- src/components/TodoItem/TodoItem.js
+++ src/components/TodoItem/TodoItem.js
@@ -1,9 +1,52 @@
-const TodoItem = ({task}) =>{
-    return (
+import {useDispatch} from "react-redux";
+import {deleteTodo, updateTodo} from "../../Action/todoAction";
+import {useRef, useState} from "react";
+
+const TodoItem = ( task ) =>{
+
+    const [isUpdate , setIsUpdate] = useState(false);
+    // console.log(task.task1.id)
+    const dispatch = useDispatch();
+    const textRef = useRef(null);
+    // console.log("entered task is ",task)
+
+    const editFormState = (e)=>{
+        e.preventDefault();
+        if (! textRef.current.value.length || /^(\w+\s)*\s*$/i.test( textRef.current.value ) ) {
+            return;
+        }
+        dispatch(updateTodo({message : textRef.current.value , id: task.task1.id}));
+        setIsUpdate(false);
+        textRef.current = null
+    }
+    const renderForm = () =>{
+        return(
+            <form onSubmit={editFormState}>
+                <input
+                    ref={textRef}
+                type='text'
+                defaultValue={task.task1.task}
+                />
+                <button type='submit' >Edit Todo</button>
+            </form>
+        )
+    }
+
+    const renderItem = () => {
+        return(
         <>
-        {task.task}
+            {task.task1.task}
+            <button onClick={() => setIsUpdate(true)}>Edit</button>
+            <button onClick={() =>dispatch(deleteTodo(task.task1.id))}>Delete</button>
+            <div></div>
+        </>
+        )}
+
+    return(
+        <>
+            <p></p>
+        {isUpdate ? renderForm() : renderItem()}
         </>
     )
 }
-
-export default TodoItem;
+export default TodoItem
